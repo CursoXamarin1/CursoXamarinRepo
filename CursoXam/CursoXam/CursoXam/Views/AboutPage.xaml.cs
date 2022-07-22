6f13487f3aafdab98da1180a5d64ad21ec8dfca6
assacm <alma_julissa@live.com.mx>
2022-07-21 22:51:48 -1100

    Creacion del proyecto Xamarin en VS

---
+++ CursoXam/CursoXam/CursoXam/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CursoXam.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
